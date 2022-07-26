23b6334dcffe779b0c08cf9f4bf7958ee707f9c8
nikhillondhe9 <nikhillondhe9@gmail.com>
2022-07-25 21:25:11 -0400

    strings and working with strings

---
+++ SummarizingText/SummarizingText/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Text;
+
+
+namespace SummarizingText
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var sentence = "This is a really really really really really really a long sentence...";
+
+            //Console.WriteLine( StringUtility.SummarizeText(sentence));
+            var builder = new StringBuilder("Hello World");
+            
+            builder.Append('-', 10);
+            builder.AppendLine();
+            builder.Append("Header")
+            .AppendLine()
+            .Append('-', 10);
+
+
+            builder.Replace('-', '+');
+
+            builder.Remove(0, 10)
+                .Insert(0, new String('-', 10));
+
+            Console.WriteLine(builder[23]);
+
+            Console.WriteLine(builder);
+
+
+
+        }
+    }
+}
---
+++ WorkingWithStrings/WorkingWithStrings/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace WorkingWithStrings
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var fullName = "Nikhil Londhe ";
+
+            Console.WriteLine("Trim: '{0}'", fullName.Trim());
+            Console.WriteLine("To Upper: '{0}'", fullName.ToUpper());
+            
+            var index = fullName.IndexOf(' ');
+            var firstName = fullName.Substring(0, index);
+            var lastName = fullName.Substring(index + 1);
+
+            Console.WriteLine(firstName);
+            Console.WriteLine(lastName);
+
+            var names = fullName.Split(' ');
+            Console.WriteLine(names[0]);
+            Console.WriteLine(names[1]);
+
+
+            if (String.IsNullOrWhiteSpace(" "))
+            {
+                Console.WriteLine("Invalid!");
+            }
+
+            var str = "25";
+
+            var age = Convert.ToByte(str);
+
+            Console.WriteLine(age);
+
+            float price = 29.96f;
+
+            Console.WriteLine(price.ToString("C"));
+        }
+    }
+}
