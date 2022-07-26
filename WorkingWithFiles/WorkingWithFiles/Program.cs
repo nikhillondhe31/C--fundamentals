6f57c770e24a07f01dc85bca78e3b966a5d43788
nikhillondhe9 <nikhillondhe9@gmail.com>
2022-07-25 22:06:11 -0400

    files and working with files

---
+++ WorkingWithFiles/WorkingWithFiles/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.IO;
+
+namespace WorkingWithFiles
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var path = @"C:\Users\nikhi\Desktop\";
+            /*
+            File.Copy(@"c:\temp\myfile.jpg", @"d:\temp\myfile.jpg", true);
+            File.Delete(path);
+            if (File.Exists(path))
+            {
+
+            }
+            var content = File.ReadAllText(path);
+
+            var fileInfo = new FileInfo(path);
+            fileInfo.CopyTo("...");
+            fileInfo.Delete();
+            var mybool = fileInfo.Exists;*/
+
+            //Directory
+
+            Directory.CreateDirectory(path);
+
+            var files = Directory.GetFiles(path, "*.*", SearchOption.AllDirectories);
+
+            foreach (var file in files)
+            {
+                Console.WriteLine(file);
+            }
+
+            var path2 = @"C:\Users\nikhi\Desktop\";
+
+            Console.WriteLine(Path.GetExtension(path2));
+
+        }
+    }
+}
